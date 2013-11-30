5393ad07d5937a1b864afb55f63ad1d3048aece3
tmtxt <tmtxt@Truongs-Macbook.local>
2013-11-30 15:16:21 +0700

    add libraries

---
+++ SpringDM/copy.sh
@@ -0,0 +1,3 @@
+cp /Volumes/tmtxt/sa-chay/Test/target/Drug-1.0-SNAPSHOT.jar /Volumes/tmtxt/felix-std/
+cp /Volumes/tmtxt/sa-chay/DAO/target/DAO-1.0-SNAPSHOT.jar /Volumes/tmtxt/felix-std/
+cp /Volumes/tmtxt/sa-chay/DrugGroup/target/DrugGroup-1.0-SNAPSHOT.jar /Volumes/tmtxt/felix-std/
